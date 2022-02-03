62df2c5820ff428c3a986af0f56a6943745bb91e
tpan <thomas.pan@lendesk.com>
2022-02-03 14:46:55 -0800

    Handle whitespace simply

--- slackmoji.js
+++ slackmoji.js
@@ -1,16 +1,20 @@
-const readline = require('readline');
+const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question(`Provide a string to translate: `, input => {
-  console.log(slackTranslate(input))
-  rl.close()
-})
+rl.question(`Provide a string to translate: `, (input) => {
+  console.log(slackTranslate(input));
+  rl.close();
+});
 
-function slackTranslate(string){
-  return string.split("").map((char)=>{
-    return `:alphabet-white-${char}:`
-  }).join("")
+function slackTranslate(string) {
+  return string
+    .split("")
+    .map((char) => {
+      if (/^\s*$/.test(char)) return ":white_square:";
+      else return `:alphabet-white-${char}:`;
+    })
+    .join("");
 }
