34116e5cd861c9242b8dc8bfa491726ee2b74f4a
tpan <thomas.pan@lendesk.com>
2022-02-03 13:14:42 -0800

    lets make it a cli tool instead idk

--- slackmoji.js
+++ slackmoji.js
@@ -1,7 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question(`Provide a string to translate: `, input => {
+  console.log(slackTranslate(input))
+  rl.close()
+})
+
 function slackTranslate(string){
   return string.split("").map((char)=>{
     return `:alphabet-white-${char}:`
   }).join("")
 }
-
-console.log(slackTranslate("yeet"))
